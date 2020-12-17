5d53f6d0da50a7adc48a2450a05f322f99ea0262
Phil Jirsa <phil.jirsa@microsoft.com>
2020-12-17 15:13:53 -0600

    add day2 part1, refactor day1

--- Program.cs
+++ Program.cs
@@ -2,72 +2,33 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace advent_of_code
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var results = ReadInputFile("input1.txt");
-
-            Console.WriteLine($"Found {results.Count()} results");
-          
-            var product = FindPairs(results);
-            Console.WriteLine(product);
-
-            var trioproduct = FindTrio(results, 2020);
-            Console.WriteLine(trioproduct);
+            new Program().RunAsync().Wait();
         }
 
-        private static int[] ReadInputFile(string filename)
+        private async Task RunAsync()
         {
-            var fileContents = File.ReadAllLines(filename);
+            var day1input = await ReadInputFileAsync("input1.txt");
+            var day1 = new Day1();
+            day1.Run(day1input);
 
-            return fileContents.Select(s => Int32.Parse(s)).ToArray();
+            var day2input = await ReadInputFileAsync("input2.txt");
+            var day2 = new Day2();
+            day2.Run(day2input);
         }
 
-        private static int FindPairs(int[] values)
+        private async Task<string[]> ReadInputFileAsync(string filename)
         {
-            values = values.OrderBy(s => s).ToArray();
-
-            for (int i = 0; i < values.Length / 2; i++)
-            {
-                for (int j = values.Length - 1; j >=0; j--)
-                {
-                    int sum = values[i] + values[j];
-                    
-                    if (sum == 2020)
-                    {
-                        Console.WriteLine($"{values[i]} + {values[j]} = {sum}");
-                        return values[i] * values[j];
-                    }
-                }
-            }
-            
-            return 0;
+            var fileContents = await File.ReadAllLinesAsync(filename);
+            return fileContents.ToArray();
         }
-
-        private static int FindTrio(int[] values, int searchValue)
-        {
-            for (int i = 0; i < values.Length; i++)
-            {
-                for (int j = 1; j < values.Length; j++)
-                {
-                    for (int k = 2; k < values.Length; k++)
-                    {
-                        int sum = values[i] + values[j] + values[k];
-                        if (sum == searchValue)
-                        {
-                            Console.WriteLine($"{values[i]} + {values[j]} + {values[k]} = {sum}");
-                            return values[i] * values[j] * values[k];
-                        }
-                    }
-                }
-            }
-
-            return 0;
-        }
+        
     }
 }
